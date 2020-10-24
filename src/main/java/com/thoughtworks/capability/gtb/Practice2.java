9d0066e0689f3184bfd29b5b25684dd6b2b3d7eb
di.qin <di.qin@thoughtworks.com>
2020-10-24 10:30:43 +0800

    [qd]: finish system.

--- src/main/java/com/thoughtworks/capability/gtb/Practice2.java
+++ src/main/java/com/thoughtworks/capability/gtb/Practice2.java
@@ -10,18 +10,18 @@ import java.time.LocalDate;
  */
 public class Practice2 {
 
-  public static final int FRIDAY_TO_ADD = 3;
-  public static final int SATURDAY_TO_ADD = 2;
-  public static final int WORK_DAY_TO_ADD = 1;
+    public static final int FRIDAY_TO_ADD = 3;
+    public static final int SATURDAY_TO_ADD = 2;
+    public static final int WORK_DAY_TO_ADD = 1;
 
-  public static LocalDate getNextWorkDate(LocalDate date) {
-    switch (date.getDayOfWeek()){
-      case FRIDAY:
-        return date.plusDays(FRIDAY_TO_ADD);
-      case SATURDAY:
-        return date.plusDays(SATURDAY_TO_ADD);
-      default:
-        return date.plusDays(WORK_DAY_TO_ADD);
+    public static LocalDate getNextWorkDate(LocalDate date) {
+        switch (date.getDayOfWeek()) {
+            case FRIDAY:
+                return date.plusDays(FRIDAY_TO_ADD);
+            case SATURDAY:
+                return date.plusDays(SATURDAY_TO_ADD);
+            default:
+                return date.plusDays(WORK_DAY_TO_ADD);
+        }
     }
-  }
 }
